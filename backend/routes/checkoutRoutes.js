7332b0cd1950c6e33c6bec285b0c94801e61a63a
Srijith Mulupuri <165108998+Srijith1912@users.noreply.github.com>
2026-03-18 11:13:22 -0700

    Delete backend directory

--- backend/routes/checkoutRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {checkout, orderHistory}  = require('../controllers/checkoutController');
-
-router.post("/", checkout);
-router.get("/history", orderHistory);
-
-module.exports = router;
