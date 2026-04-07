54a84ac0297d27b7fde2a3f94684ac6e2625a79e
Srijith Mulupuri <mulupurisrijith@gmail.com>
2026-04-06 20:43:49 -0700

    Pre-deployment

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const itemRoutes = require("./routes/itemRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000' }));
 app.use(express.json());
 
 app.use("/inventory", itemRoutes);
@@ -15,7 +15,7 @@ app.use("/checkout", checkoutRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
